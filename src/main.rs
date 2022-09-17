b40262975f37f74b51d994107274c95dbae86b7a
Njagi Mwaniki <njagi@urbanslug.com>
2022-09-17 15:19:30 +0200

    Use i and j pointers instead of iterators
    add start and accepting states

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,51 @@
-use eds::{EDT, Letter};
+use eds::EDT;
 use petgraph::Graph;
 use petgraph::graph::NodeIndex;
-use std::collections::VecDeque;
 use std::collections::HashSet;
-use std::slice;
+use petgraph::dot::{Dot, Config};
 
-fn build_compacted_trie(edt: EDT) -> Graph<(usize, usize, usize), Vec<u8>>{
-    let mut automaton = Graph::<(usize, usize, usize), Vec<u8>>::new();
 
+fn build_automaton(edt: EDT) -> Graph<(usize, usize, usize), Vec<u8>>{
     let diameter =  edt.w();
-    let mut solid_iterator: slice::Iter<(usize, usize)> =  edt.get_solid_intervals().iter();
-    let mut degenerates_iterator: slice::Iter<(usize, usize)> = edt.get_degenerate_letters().iter();
+    let solids: &Vec<(usize, usize)> =  edt.get_solid_intervals();
+    let degenerates: &Vec<(usize, usize)> = edt.get_degenerate_letters();
 
-    // queue to sync the results of the two iterators in ascending order
-    let mut queue: VecDeque<(usize, usize, Letter)> = VecDeque::new();
+    let mut i: usize = 0; // solids_pointer
+    let mut j: usize = 0; // degenerates_pointer
 
+    let mut automaton = Graph::<(usize, usize, usize), Vec<u8>>::new();
     // temporary buffer to store the nodes from the previous iteration of the loop
     // from_nodes
     let mut stored = HashSet::<(NodeIndex, usize)>::new();
 
-    // Loop over the contents of the queue and build an automaton out of them
-    loop {
-        let (solid_start, solid_end) = *solid_iterator.next().unwrap_or(&(usize::MAX, usize::MAX));
-        let (degenerate_start, degenerate_end) = *degenerates_iterator.next().unwrap_or(&(usize::MAX, usize::MAX));
+    // start node
+    let start_node = automaton.add_node((0, 0, 0));
+    stored.insert(((start_node), 0));
 
-        let (start, stop, _) = if queue.front().is_some() {
-            if degenerate_start != usize::MAX {
-                queue.push_back((degenerate_start, degenerate_end, Letter::Degenerate));
-            }
-            if solid_start != usize::MAX {
-                queue.push_back((solid_start, solid_end, Letter::Solid));
+    while i < solids.len() || j < degenerates.len() {
+        let (start, stop) = if i < solids.len() && j < degenerates.len() {
+            let (s_s, s_e): (usize, usize) = solids[i];
+            let (d_s, d_e): (usize, usize) = degenerates[j];
+
+            if s_s < d_s {
+                i += 1;
+                (s_s, s_e)
+            } else {
+                j += 1;
+                (d_s, d_e)
             }
 
-            queue.pop_front().unwrap()
-        } else if solid_start < degenerate_start {
-            if degenerate_start != usize::MAX {
-                queue.push_back((degenerate_start, degenerate_end, Letter::Degenerate));
-            }
-            (solid_start, solid_end, Letter::Solid)
+
+        } else if i >= solids.len() {
+            let (d_s, d_e): (usize, usize) = degenerates[j];
+            j += 1;
+            (d_s, d_e)
         } else {
-            if solid_start != usize::MAX {
-                queue.push_back((solid_start, solid_end, Letter::Solid));
-            }
-            (degenerate_start, degenerate_end, Letter::Degenerate)
+            let (s_s, s_e): (usize, usize) = solids[i];
+            i += 1;
+            (s_s, s_e)
         };
+
 
         if stop <= diameter {
             let h = edt[stop-1].len();
@@ -61,46 +63,67 @@ fn build_compacted_trie(edt: EDT) -> Graph<(usize, usize, usize), Vec<u8>>{
                 })
                 .collect::<Vec<(NodeIndex, NodeIndex)>>();
 
-            stored.retain(|(_, v)|  *v == stop-1);
+            stored.clear();
 
             nodes_added.iter().for_each(|v| { stored.insert(*v); } );
             automaton.extend_with_edges(edges);
         }
 
-        if queue.is_empty() {
-            break;
-        }
     }
-    automaton
+
+
+    let accepting_state = automaton.add_node((diameter, diameter, 0));
     
+
+    let edges = stored
+        .iter()
+        .map(|(from_node, _)| (*from_node, accepting_state))
+        .collect::<Vec<(NodeIndex, NodeIndex)>>();
+
+    automaton.extend_with_edges(edges);
+
+    automaton
 }
 
+
+fn print_dot(automaton: Graph<(usize, usize, usize), Vec<u8>>) {
+    print!("digraph {{");
+    print!("
+rankdir=LR
+node [shape=box]
+");
+    print!("{:?}", Dot::with_config(&automaton, &[Config::GraphContentOnly, Config::EdgeNoLabel]));
+    println!("}}");
+    println!()
+
+}
+
+
 fn main() {
-    let ed_string = "A{T,G}{C,A}{T,A}TC";
+    // let ed_string = "A{T,G}{C,A}{T,A}TC";
     // let ed_string = EDT::from_str("ACTA{ATC,CGA}{ACGT,GCGC}A{CTA,C,}A");
     // let edt = EDT::from_str("ACTA{ATC,CGA}C{ACGT,GCGC}A");
-
-    build_compacted_trie(EDT::from_str(ed_string));
     
+    // build_compacted_trie(EDT::from_str(ed_string));
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use petgraph::dot::{Dot, Config};
-
     #[test]
     fn test_build_compacted_trie() {
         let ed_string = "A{T,G}{C,A}{T,A}TC";
-        // let ed_string = EDT::from_str("ACTA{ATC,CGA}{ACGT,GCGC}A{CTA,C,}A");
-        // let edt = EDT::from_str("ACTA{ATC,CGA}C{ACGT,GCGC}A");
+        // let ed_string = "ACTA{ATC,CGA}{ACGT,GCGC}A{CTA,C,}A";
+        // let ed_string = "ACTA{ATC,CGA}C{ACGT,GCGC}A";
+        let ed_string = "{ATC,CGA}{ACGT,GCGC}A{CTA,C,}A{C,GTA,}";
+        // let ed_string = EDT::from_str("ACTA{ATC,CGA}{ACGT,GCGC}A{CTA,C,}A{C,GTA}");
+
 
         let edt = EDT::from_str(ed_string);
         println!("\n{edt}");
 
-        let automaton = build_compacted_trie(edt);
-        println!("{:?}", Dot::with_config(&automaton, &[Config::EdgeNoLabel]));
-
+        let automaton = build_automaton(edt);
+        print_dot(automaton);
     }
 }
