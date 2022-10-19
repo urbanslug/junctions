c732ee9d22eb8052d855ce02663d26bb37c0b5ba
Njagi Mwaniki <njagi@urbanslug.com>
2022-10-19 10:32:54 +0200

    Start on improved version
    Style improvements

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,8 @@
 // use petgraph::{Graph, data::Build};
 // use generalized_suffix_tree::GeneralizedSuffixTree;
 
-
-mod utils;
-mod types;
 mod naive;
+mod types;
+mod utils;
 
-
-
-fn main() {
-    // let ed_string = "A{T,G}{C,A}{T,A}TC";
-    // let ed_string = EDT::from_str("ACTA{ATC,CGA}{ACGT,GCGC}A{CTA,C,}A");
-    // let edt = EDT::from_str("ACTA{ATC,CGA}C{ACGT,GCGC}A");
-
-    // build_compacted_trie(EDT::from_str(ed_string));
-    
-}
-
-
-
-
+fn main() {}
--- src/utils.rs
+++ src/utils.rs
@@ -1,23 +1,22 @@
 use eds::EDT;
+use generalized_suffix_tree::GeneralizedSuffixTree;
+use petgraph::dot::{Config, Dot};
+use petgraph::graph::NodeIndex;
+use petgraph::{data::Build, Graph};
 use std::cmp::min;
 use std::collections::HashSet;
-use petgraph::graph::NodeIndex;
-use petgraph::dot::{Dot, Config};
-use petgraph::{Graph, data::Build};
-use generalized_suffix_tree::GeneralizedSuffixTree;
 
-use ndarray_to_img::plot::{self, Plottable};
 use ndarray::{Array, Array2};
+use ndarray_to_img::plot::{self, Plottable};
 
 use crate::types::Tree;
 
 const ADD_Q_0: bool = true;
 const ADD_ACCEPTING_STATE: bool = false;
 
-
 pub fn build_automaton(edt: EDT) -> Graph<usize, String> {
-    let diameter =  edt.w();
-    let solids: &Vec<(usize, usize)> =  edt.get_solid_intervals();
+    let diameter = edt.w();
+    let solids: &Vec<(usize, usize)> = edt.get_solid_intervals();
     let degenerates: &Vec<(usize, usize)> = edt.get_degenerate_letters();
 
     let mut i: usize = 0; // solids_pointer
@@ -37,7 +36,6 @@ pub fn build_automaton(edt: EDT) -> Graph<usize, String> {
     }
 
     while i < solids.len() || j < degenerates.len() {
-
         // figure out what we are iterating through
         let (start, stop) = if i < solids.len() && j < degenerates.len() {
             let (s_s, s_e): (usize, usize) = solids[i];
@@ -50,8 +48,6 @@ pub fn build_automaton(edt: EDT) -> Graph<usize, String> {
                 j += 1;
                 (d_s, d_e)
             }
-
-
         } else if i >= solids.len() {
             let (d_s, d_e): (usize, usize) = degenerates[j];
             j += 1;
@@ -62,16 +58,15 @@ pub fn build_automaton(edt: EDT) -> Graph<usize, String> {
             (s_s, s_e)
         };
 
-
         // add nodes or edges for that degenerate letter or solid string
         if stop <= diameter {
-            let h = edt[stop-1].len();
+            let h = edt[stop - 1].len();
 
             let edge_labels = (0..h)
                 .map(|element| {
                     // let node = automaton.add_node((n, element));
                     let mut seed: String = (start..stop)
-                        .map(|col|  edt.base_at([col, element]) as char )
+                        .map(|col| edt.base_at([col, element]) as char)
                         .collect::<String>();
                     seed.retain(|c| c != '*');
                     if seed == String::from("") {
@@ -83,7 +78,7 @@ pub fn build_automaton(edt: EDT) -> Graph<usize, String> {
 
             if edge_labels.len() == 1 {
                 // solid
-                let previous_node =  maybe_previous_node.unwrap();
+                let previous_node = maybe_previous_node.unwrap();
                 let current_node = automaton.add_node(n);
                 let edges = edge_labels
                     .iter()
@@ -93,9 +88,12 @@ pub fn build_automaton(edt: EDT) -> Graph<usize, String> {
                 automaton.extend_with_edges(edges);
                 maybe_previous_node.replace(current_node);
             } else {
-                let previous_node =  maybe_previous_node.unwrap();
+                let previous_node = maybe_previous_node.unwrap();
 
-                let labels_vec = edge_labels.iter().map(|l| l.as_str() ).collect::<Vec<&str>>();
+                let labels_vec = edge_labels
+                    .iter()
+                    .map(|l| l.as_str())
+                    .collect::<Vec<&str>>();
 
                 let mut trie = Tree {
                     root: previous_node,
@@ -117,38 +115,41 @@ pub fn build_automaton(edt: EDT) -> Graph<usize, String> {
     automaton
 }
 
-
+/// Print the dot output to stdout
 pub fn print_dot(automaton: &Graph<usize, String>, input_str: &str) {
     print!("digraph {{");
-    print!("
+    print!(
+        "
 graph [label=\"{}\", labelloc=t, fontsize=12];
 rankdir=LR
 node [shape=circle]
-", input_str);
-    print!("{:?}", Dot::with_config(&automaton, &[Config::GraphContentOnly]));
+",
+        input_str
+    );
+    print!(
+        "{:?}",
+        Dot::with_config(&automaton, &[Config::GraphContentOnly])
+    );
     println!("}}");
     println!()
 }
-
 
 fn build_generalized_suffix_tree(text: Vec<&str>) {
     let mut tree = GeneralizedSuffixTree::new();
 
     text.into_iter().enumerate().for_each(|(idx, s)| {
         tree.add_string(String::from(s), (idx as u8) as char);
-    }
-    );
+    });
     tree.pretty_print();
     println!("{}", tree.is_suffix("BCE"));
 }
 
 // TODO: handle duplicate string
 fn build_radix_tree(automaton: &mut Tree, text: &Vec<&str>, id: usize, add_hard_node: bool) {
-
-
     // let mut text: Vec<&[u8]> = text.iter().map(|s| s.as_bytes()).collect();
-    let mut text: Vec<Vec<char>> = text.iter()
-        .map(|s| s.chars().collect::<Vec<char>>() )
+    let mut text: Vec<Vec<char>> = text
+        .iter()
+        .map(|s| s.chars().collect::<Vec<char>>())
         .collect();
     text.sort();
 
@@ -163,7 +164,7 @@ fn build_radix_tree(automaton: &mut Tree, text: &Vec<&str>, id: usize, add_hard_
 
     while i < text.len() - 1 {
         let f = text.get(i).unwrap();
-        let s = text.get(i+1).unwrap();
+        let s = text.get(i + 1).unwrap();
 
         let f_len = f.len();
         let s_len = s.len();
@@ -179,7 +180,7 @@ fn build_radix_tree(automaton: &mut Tree, text: &Vec<&str>, id: usize, add_hard_
         //     s_len);
 
         loop {
-            if  j < min(f_len, s_len) &&  f[j] == s[j] {
+            if j < min(f_len, s_len) && f[j] == s[j] {
                 string_buffer.push(f[j] as char);
             } else {
                 let mut h = HashSet::<(NodeIndex, NodeIndex, String)>::new();
@@ -188,7 +189,7 @@ fn build_radix_tree(automaton: &mut Tree, text: &Vec<&str>, id: usize, add_hard_
 
                 // replace with i_path check
                 //if  trie.edges(root_node).count() == 0 {
-                if  i_path.is_empty() {
+                if i_path.is_empty() {
                     //let internal_node = trie.add_node(id);
                     let internal_node = root_node;
 
@@ -203,18 +204,15 @@ fn build_radix_tree(automaton: &mut Tree, text: &Vec<&str>, id: usize, add_hard_
                     // i_path.push((root_node, internal_node, string_buffer.clone()));
                     i_path.push((internal_node, leaf_s, s_suffix.clone()));
 
-
                     trie.extend_with_edges(h);
 
                     leaves.insert(leaf_f);
                     leaves.insert(leaf_s);
-
                 } else {
                     // walk down the trie following the path of the ith string
                     let mut k: usize = 0; // TODO: rename furthest similar char
                     let mut furthest_edge: usize = 0;
                     for (_, _, s) in i_path.iter() {
-
                         k += s.len();
 
                         if k >= j {
@@ -227,7 +225,7 @@ fn build_radix_tree(automaton: &mut Tree, text: &Vec<&str>, id: usize, add_hard_
                     dbg!(k, j);
 
                     if furthest_edge >= i_path.len() {
-                        let (i, o, s) = i_path[furthest_edge-1].clone();
+                        let (i, o, s) = i_path[furthest_edge - 1].clone();
 
                         // let internal_node = trie.add_node(id);
                         let leaf_s = trie.add_node(id);
@@ -239,12 +237,10 @@ fn build_radix_tree(automaton: &mut Tree, text: &Vec<&str>, id: usize, add_hard_
                         i_path.push((o, leaf_s, s_suffix.clone()));
                         trie.extend_with_edges(h);
 
-
                         // update leaves
                         leaves.remove(&o);
                         leaves.insert(leaf_f);
                         leaves.insert(leaf_s);
-
                     } else {
                         // break the current furthest edge
                         let (i, o, s) = i_path[furthest_edge].clone();
@@ -285,39 +281,36 @@ fn build_radix_tree(automaton: &mut Tree, text: &Vec<&str>, id: usize, add_hard_
 
             j += 1;
         }
-        i+= 1;
+        i += 1;
     }
 
     let hard_node = trie.add_node(id);
     automaton.hard_node = Some(hard_node);
 
     if add_hard_node {
-        let edges = leaves.iter().map(|leaf_node| (*leaf_node, hard_node, String::new() ) );
+        let edges = leaves
+            .iter()
+            .map(|leaf_node| (*leaf_node, hard_node, String::new()));
         trie.extend_with_edges(edges);
     }
 }
 
-
 pub fn visualize_matrix(matrix: &Array2<i32>, image_name: &str) {
-    
-    let config =  ndarray_to_img::Config {
-	      verbosity: 0,
-	      with_color: false,
-	      annotate_image: true,
-	      draw_diagonal: false,
-	      draw_boundaries: true,
-	      scaling_factor: 50,
+    let config = ndarray_to_img::Config {
+        verbosity: 0,
+        with_color: false,
+        annotate_image: true,
+        draw_diagonal: false,
+        draw_boundaries: true,
+        scaling_factor: 50,
     };
-
 
     let matrix = plot::Matrix {
         matrix: matrix.clone(),
     };
 
-	  let scaled_matrix: plot::Matrix<i32> = matrix.scale_matrix(&config);
+    let scaled_matrix: plot::Matrix<i32> = matrix.scale_matrix(&config);
     assert_eq!(scaled_matrix.plot(&config, image_name).unwrap(), ());
-
-
 }
 
 #[cfg(test)]
@@ -331,7 +324,6 @@ mod tests {
         // let ed_string = "ACTA{ATC,CGA}C{ACGT,GCGC}A";
         let ed_string = "{ATC,CGA}{ACGT,GCGC}A{CTA,C,}A{C,GTA,}";
         // let ed_string = EDT::from_str("ACTA{ATC,CGA}{ACGT,GCGC}A{CTA,C,}A{C,GTA}");
-
 
         let edt = EDT::from_str(ed_string);
         println!("\n{edt}");
@@ -370,10 +362,9 @@ mod tests {
         for leaf in trie.leaves.iter() {
             trie.graph
                 .edges_directed(*leaf, petgraph::Incoming)
-                .for_each(|e| { dbg!(e); } );
+                .for_each(|e| {
+                    dbg!(e);
+                });
         }
-
     }
-
-
 }
