19ee866c5f491385cfb5994b261647a98bde2e90
Njagi Mwaniki <njagi@urbanslug.com>
2022-11-16 18:14:19 +0100

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -18,68 +18,52 @@ void test_handle_epsilon();
 void test_contains_intersect();
 void test_parse_ed_string();
 
-// remove row_idx_range
-// should be within the same
-bool is_letter_matched (EDS &eds, matrix &dp_matrix, int letter_idx,
-                            std::vector<int> &row_idx_range) {
-  std::vector<span> letter_span = eds.str_offsets[letter_idx];
 
-  for (int row_idx : row_idx_range) {
-    for (auto sp : letter_span) {
-      if (dp_matrix[row_idx][sp.stop]) {
-        return true;
-      }
+// is at least one the strings in the letter_idx index of the eds given matched?
+// it is matched is at least one of its indexes is true in the dp matrix
+bool is_letter_matched(EDS &eds, // the eds we are checking
+                       int letter_idx, // has any string in this d letter been matched
+                       matrix &dp_matrix, // matrix that tracks matches in the eds
+                       int row_idx // row in dp_matrix
+                       ) {
+  std::vector<span> &letter_span = eds.str_offsets[letter_idx];
+
+  for (auto sp : letter_span) {
+    if (dp_matrix[row_idx][sp.stop]) {
+      return true;
     }
   }
 
   return false;
 };
 
-bool can_extend(EDS &eds, matrix &dp_matrix, int pos, std::string &j_str,
-                size_t curr, // the letter whose chars we are matching in
-                size_t comp  // the letter whose strings we are matching against
-                ) {
-  if (DEBUG_LEVEL > 4) {
-        printf("\t\t[cpp::main::intersect::can_extend]\n");
-      }
+// is at least one the strings in the letter_idx index of the eds given matched?
+// it is matched is at least one of its indexes is true in the dp matrix
+bool is_prev_letter_matched(
+    EDS &eds,          // the eds we are checking
+    int letter_idx,    // has any string in this d letter been matched
+    matrix &dp_matrix, // matrix that tracks matches in the eds
+    int row_idx        // row in dp_matrix
+){
 
-      // printf("\t\t\tpos: %d\n", pos);
+  if (row_idx == 0) {
+    return true;
+  }
 
-      // TODO: maybe needlessly reptitive & assignments
-      size_t letter_start_in_n = eds.str_offsets[comp][0].start;
-      size_t start = pos;
+  return is_letter_matched(eds, letter_idx, dp_matrix, row_idx-1);
+};
 
-      start += letter_start_in_n;
-
-      // TODO: is it better to call  compute_str_starts ?
-      // is start in the start of a degenerate letter?
-      bool check_prev_letter = false;
-      for (auto sp : eds.str_offsets[comp]) {
-        // printf("(%lu, %lu)", s.start, start);
-        if (sp.start == start) {
-          check_prev_letter = true;
-          break;
-        }
-      }
-
-      // printf("\t\t\tletter start: %lu pos %d curr start %lu prev : %d\n",
-      //       letter_start_in_n, pos, start, check_prev_letter);
-
-      if (check_prev_letter) {
-        // can we extend from the previous degenerate letter?
-        for (auto sp : eds.str_offsets[comp - 1]) {
-          if (dp_matrix[curr][sp.stop]) {
-            return true;
-          }
-        }
-      } else {
-        // can we extend from within this current degenerate letter?
-        if (dp_matrix[curr][start - 1]) {
-          return true;
-        }
-      }
-
-      return false;
+bool is_prefix_matched(EDS &eds, //
+                       matrix &dp_matrix, //
+                       int pos, // the position in which our match started
+                       size_t row, // the letter whose chars we are matching in
+                       size_t comp  // the letter whose strings we are matching against
+                       ) {
+  // TODO: should this be a one liner?
+  // can we extend from within this current degenerate letter?
+  size_t letter_start_in_size = eds.str_offsets[comp].front().start;
+  size_t match_start_in_size = pos + letter_start_in_size;
+  return dp_matrix[row][match_start_in_size - 1];
 }
 
     /*
@@ -178,8 +162,7 @@ bool can_extend(EDS &eds, matrix &dp_matrix, int pos, std::string &j_str,
       ---------------------
      */
 
-    
-
+    std::vector<int> q_end_indexes, w_end_indexes; // TODO: allocate one large one
     size_t i, j;
     i = j = 0;
     bool inc_i = false, inc_j = false;
@@ -191,29 +174,25 @@ bool can_extend(EDS &eds, matrix &dp_matrix, int pos, std::string &j_str,
 
       inc_i = inc_j = false;
       std::pair<STvertex, std::string> root_text;
+
+      // -------
+      //
+      // -------
+
+      q_end_indexes.clear();
       root_text = q_suffix_trees[j];
 
       if (DEBUG_LEVEL > 3) {
         printf("\tj text: %s\n", root_text.second.c_str());
       }
 
-      if (eds_w.data[i].has_epsilon) {
+      if (eds_w.data[i].has_epsilon && (j == 0 || is_letter_matched(eds_w, i - 1, w_matrix, j))) {
         std::vector<span> letter_span = eds_w.str_offsets[i];
         int epsilon_idx = letter_span.back().stop;
 
-        if (j == 0) {
-          w_matrix[j][epsilon_idx] = true;
-          inc_i = true;
-        } else {
-          std::vector<int> row_idx_range;
-          row_idx_range.push_back(j);
-          row_idx_range.push_back(j - 1);
-
-          if ((is_letter_matched(eds_w, w_matrix, i - 1, row_idx_range))) {
-            w_matrix[j][epsilon_idx] = true;
-            inc_i = true;
-          }
-        }
+        w_matrix[j][epsilon_idx] = true;
+        q_end_indexes.push_back(epsilon_idx);
+        inc_i = true;
       }
 
       for (auto i_str : eds_w.data[i].data) {
@@ -221,80 +200,75 @@ bool can_extend(EDS &eds, matrix &dp_matrix, int pos, std::string &j_str,
 
         vector<int> res = FindEndIndexes(i_str.c_str(), &root_text.first, root_text.second.c_str());
 
-        if (res.empty()) {
-          continue;
-        } // if pos is not found
+        // if the string does not match anywhere
+        if (res.empty()) { continue; }
 
         for (int pos: res) {
-          if (DEBUG_LEVEL > 3) {
-            printf("\t\ti_str: %s pos: %d\n", i_str.c_str(), pos);
-          }
 
-          if (i == 0) {
-            int end_idx = pos + i_str.length() - 1;
-            q_matrix[i][end_idx] = true;
-            inc_i = true;
-            continue;
-          }
+          if (DEBUG_LEVEL > 3) { printf("\t\ti_str: %s pos: %d\n", i_str.c_str(), pos); }
 
-          if ((can_extend(eds_q, q_matrix, pos, i_str, i, j))) {
-            size_t pos_in_n = eds_q.str_offsets[j][0].start + pos;
-            q_matrix[i][pos_in_n] = true;
-            inc_i = true;
-
-            if (DEBUG_LEVEL > 4) {
-              printf("\t\tpos %d  \tpos_in_n: %lu\n", pos, pos_in_n);
+          // TODO: offload to lambda
+          // filter results from ST
+          bool invalid_pos = false;
+          int letter_start_in_n = eds_q.str_offsets[j].front().start;
+          for (auto sp : eds_q.str_offsets[j]) {
+            int k = letter_start_in_n + pos;
+            if (sp.stop >= k && (k + i_str.length() - 1) > sp.stop) {
+              invalid_pos = true;
             }
+          }
+
+          if (invalid_pos) { continue; }
+
+
+          if (i == 0 ||
+              is_letter_matched(eds_q, j - 1, q_matrix, i) ||
+              is_prefix_matched(eds_q, q_matrix, pos, i, j)
+              ) {
+            int end_idx = letter_start_in_n + pos + i_str.length() - 1;
+            q_matrix[i][end_idx] = true;
+            q_end_indexes.push_back(end_idx);
+            inc_i = true;
+
+            if (DEBUG_LEVEL > 4) { printf("\t\tpos %d  \tpos_in_n: %d\n", pos, end_idx); }
           }
         }
       }
 
+      // -------
+      //
+      // -------
+
+      w_end_indexes.clear();
       root_text = w_suffix_trees[i];
       if (DEBUG_LEVEL > 3) {
         printf("\ti text: %s\n", root_text.second.c_str());
       }
 
-      if (eds_q.data[j].has_epsilon) {
+      if (eds_q.data[j].has_epsilon &&
+          (i == 0 || is_letter_matched(eds_q, j - 1, q_matrix, i))) {
         std::vector<span> letter_span = eds_q.str_offsets[j];
         int epsilon_idx = letter_span.back().stop;
 
-
-        if (i == 0) {
-          q_matrix[i][epsilon_idx] = true;
-          inc_j = true;
-        } else {
-          std::vector<int> row_idx_range;
-          row_idx_range.push_back(i);
-          row_idx_range.push_back(i - 1);
-
-          if ((is_letter_matched(eds_q, q_matrix, j - 1, row_idx_range))) {
-            q_matrix[i][epsilon_idx] = true;
-            inc_j = true;
-          }
-        }
+        q_matrix[i][epsilon_idx] = true;
+        w_end_indexes.push_back(epsilon_idx);
+        inc_j = true;
       }
 
       std::vector<std::string> j_strings =  eds_q.data[j].data;
 
-
       for (auto j_str : j_strings) {
 
-        vector<int> res = FindEndIndexes(j_str.c_str(), &root_text.first,
-                               root_text.second.c_str());
+        vector<int> res = FindEndIndexes(j_str.c_str(), &root_text.first, root_text.second.c_str());
 
-        if (res.empty()) {
-          continue;
-        } // if pos is not found
+        // if the string does not match anywhere
+        if (res.empty()) { continue; }
 
-        if (DEBUG_LEVEL > 3) {
-          printf("\t\tj_str: %s\n", j_str.c_str());
-        }
+        if (DEBUG_LEVEL > 3) { printf("\t\tj_str: %s\n", j_str.c_str()); }
 
         for (int pos : res) {
 
-          if (DEBUG_LEVEL > 3) {
-            printf("\t\t\tpos: %d\n", pos);
-          }
+          if (DEBUG_LEVEL > 3) { printf("\t\t\tpos: %d\n", pos); }
 
           // filter results from ST
           bool invalid_pos = false;
@@ -308,44 +282,42 @@ bool can_extend(EDS &eds, matrix &dp_matrix, int pos, std::string &j_str,
 
           if (invalid_pos) {continue;}
 
-          if (j == 0) {
-            int end_idx = pos + j_str.length() - 1;
+          if (j == 0 ||
+              is_letter_matched(eds_w, i - 1, w_matrix, j) || // TODO: what if i == 0?
+              is_prefix_matched(eds_w, w_matrix, pos, j, i)
+              ) {
+            int end_idx = letter_start_in_n +  pos + j_str.length() - 1;
             w_matrix[j][end_idx] = true;
-            inc_j = true;
-            continue;
-          }
 
-          if (can_extend(eds_w, w_matrix, pos, j_str, j, i)) {
-            size_t pos_in_n = eds_w.str_offsets[i][0].start + pos;
-            w_matrix[j][pos_in_n] = true;
+            w_end_indexes.push_back(end_idx);
             inc_j = true;
 
             if (DEBUG_LEVEL > 4) {
-              printf("\t\t\tset j %lu \tpos_in_n: %lu\n", j, pos_in_n);
+              printf("\t\t\tset j %lu \tpos_in_n: %d\n", j, end_idx);
             }
           }
         }
       }
 
-      if (inc_i) {
-        ++i;
-        if (i < len_w) {
-          for (int k = 0; k < size_q; k++) {
-            q_matrix[i][k] = q_matrix[i - 1][k];
+
+      auto inc_and_copy = [](matrix &dp_matrix, std::size_t* row, int max_row, std::vector<int> &cols){
+        ++(*row);
+        if (*row < max_row) {
+          for (int k : cols) {
+            dp_matrix[*row][k] = dp_matrix[*row - 1][k];
           }
         }
+      };
+
+      if (inc_i) {
+        inc_and_copy(q_matrix, &i, len_w, q_end_indexes);
       }
 
       if (inc_j) {
-        ++j;
-        if (j < len_q) {
-          for (int k = 0; k < size_w; k++) {
-            w_matrix[j][k] = w_matrix[j - 1][k];
-          }
-        }
+        inc_and_copy(w_matrix, &j, len_q, w_end_indexes);
       }
 
-      // give up early we cant extend any degenerate letter
+      // give up early because we can not extend any degenerate letter
       if (!inc_j && !inc_i) {
         return false;
       }
@@ -355,6 +327,7 @@ bool can_extend(EDS &eds, matrix &dp_matrix, int pos, std::string &j_str,
   std::vector<std::size_t> w_ends = compute_str_ends(eds_w, len_w - 1);
   std::vector<std::size_t> q_ends = compute_str_ends(eds_q, len_q - 1);
 
+  // TODO: rename
   auto foo = [](matrix &m, std::vector<size_t> ends, size_t row) -> bool {
     for (auto end : ends) {
       if (m[row][end]) {
@@ -375,7 +348,7 @@ int main() {
   test_handle_epsilon();
   test_contains_intersect();
   test_lacks_intersect();
-  test_parse_ed_string();
+  // test_parse_ed_string();
   return 0;
 }
 
@@ -431,44 +404,32 @@ void test_contains_intersect() {
 }
 
 void test_handle_epsilon() {
-  std::string ed_string_w;
-  std::string ed_string_q;
-  EDS eds_w;
-  EDS eds_q;
+  std::string ed_string_w, ed_string_q;
+  EDS eds_w, eds_q;
 
   ed_string_w = "{AT,TC}{ATC,T}";
   ed_string_q = "TC{,G}{CT,T}";
-
   eds_w = parse_ed_string(ed_string_w);
   eds_q = parse_ed_string(ed_string_q);
-
   IS_TRUE(intersect(eds_w, eds_q));
 
-  /*
   ed_string_w = "{AT,TC}{ATC,T}";
   ed_string_q = "{,G}{CT,T}";
-
   eds_w = parse_ed_string(ed_string_w);
   eds_q = parse_ed_string(ed_string_q);
-
-  IS_TRUE(intersect(eds_w, eds_q));
+  // IS_TRUE(intersect(eds_w, eds_q));
 
   ed_string_w = "{AT,TC}{ATC,}";
   ed_string_q = "{TC,G}{CT,T}";
-
   eds_w = parse_ed_string(ed_string_w);
   eds_q = parse_ed_string(ed_string_q);
+  // IS_TRUE(intersect(eds_w, eds_q));
 
-  IS_TRUE(intersect(eds_w, eds_q));
-
-  ed_string_w = "{AT,TC}{ATC,}";
+  ed_string_w = "{AT,TC}{ATC,A}";
   ed_string_q = "{,G}{CT,T}";
-
   eds_w = parse_ed_string(ed_string_w);
   eds_q = parse_ed_string(ed_string_q);
-
-  IS_TRUE(intersect(eds_w, eds_q));
-  */
+  // IS_TRUE(!intersect(eds_w, eds_q));
 }
 
 void test_lacks_intersect() {
