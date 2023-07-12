6410afb19e005a6436ec1bf06ff643a32a1c1988
Njagi Mwaniki <njagi@urbanslug.com>
2023-07-12 15:16:48 +0200

    Remove dep bits/stdc++

--- src/intersect/naive.cpp
+++ src/intersect/naive.cpp
@@ -1,5 +1,3 @@
-
-#include <bits/stdc++.h>
 #include <cstddef>
 #include <cstdio>
 #include <cstdlib>
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <bits/stdc++.h>
 #include <cstddef>
 #include <cstdio>
 #include <iostream>
